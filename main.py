263729b07589e6170c648395ce9dcdb8abd43cf9
Stefano Lori <wolliq@gmail.com>
2025-11-03 09:20:47 +0100

    check

---
+++ main.py
@@ -0,0 +1,11 @@
+from loguru import logger
+import streamlit as st
+
+
+def main():
+    logger.debug("Hello from curious-checker!")
+    st.title("Hello from curious checker!")
+
+
+if __name__ == "__main__":
+    main()
